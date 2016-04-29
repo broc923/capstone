8364aea57992993dd310f9688adfb5ec8f795692
calebmann423 <catch1caleb@gmail.com>
2016-04-29 16:39:23 -0400

    Removed Not needed Code

--- IDPrinter/MagneticStripCode.cs
+++ IDPrinter/MagneticStripCode.cs
@@ -14,27 +14,8 @@ namespace IDPrinter {
         public static byte[] read = new byte[] { 0x1B, 0x72 };
         public static byte[] write = new byte[] { 0x1B, 0x77, 0x1B, 0x73, 0x1B, 0x01 };
         public static byte[] endWrite = new byte[] { 0x3F, 0x1C };
-        public static byte[] allLEDOff = new byte[] { 0x1B, 0x81 };
-        public static byte[] allLEDOn = new byte[] { 0x1B, 0x82 };
-        public static byte[] greenLEDOn = new byte[] { 0x1B, 0x83 };
-        public static byte[] yellowLEDOn = new byte[] { 0x1B, 0x84 };
-        public static byte[] redLEDOn = new byte[] { 0x1B, 0x85 };
-        public static byte[] sensorTest = new byte[] { 0x1B, 0x86 };
-        public static byte[] ramTest = new byte[] { 0x1B, 0x87 };
-        public static byte[] eraseCard = new byte[] { 0x1B, 0x63 };
-        public static byte[] selectTrack2 = new byte[] { 0x1B, 0x62, 0xD2 };
-        byte[] selectTrack2Alt = new byte[] { 0x1B, 0x62, 0x4B };
-        byte[] selectTrack1 = new byte[] { 0x1B, 0x66, 0xD2 };
-        byte[] selectTrack1Alt = new byte[] { 0x1B, 0x66, 0x4B };
-        byte[] selectTrack3 = new byte[] { 0x1B, 0x67, 0xD2 };
-        byte[] selectTrack3Alt = new byte[] { 0x1B, 0x67, 0x4B };
-        byte[] getDeviceData = new byte[] { 0x1B, 0x74 };
-        byte[] getFirmwareVersion = new byte[] { 0x1B, 0x75 };
-        byte[] getCompatibleFirmwareVersion = new byte[] { 0x1B, 0x76 };
+        public static byte[] selectTrack1 = new byte[] { 0x1B, 0x66, 0xD2 };
         public static byte[] setHiCo = new byte[] { 0x1B, 0x78 };
-        byte[] setLowCo = new byte[] { 0x1B, 0x79 };
-        byte[] getHiCoLowCo = new byte[] { 0x1B, 0x64 };
-        public static byte[] detectEZWriter = new byte[] { 0x39 };
         public static string returnString = "";
         static SerialPort serialPort;
 
